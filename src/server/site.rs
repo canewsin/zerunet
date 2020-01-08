10c7912fedf4a3de55a6c918099c716a52272429
Ansho Rei <anshorei@zeroid.bit>
2020-01-08 19:45:56 +0100

    use filegetrequest when serving file directly

--- src/server/site.rs
+++ src/server/site.rs
@@ -1,9 +1,11 @@
 use crate::error::Error;
 use actix_files::NamedFile;
 use actix_web::{web::Data, HttpRequest, Result};
+use futures::executor::block_on;
 use log::*;
 use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
+use std::str::FromStr;
 
 #[derive(Serialize, Deserialize)]
 pub struct Info {
@@ -12,7 +14,7 @@ pub struct Info {
 
 pub fn serve_file(
 	req: &HttpRequest,
-	_data: Data<crate::server::ZeroServer>,
+	data: Data<crate::server::ZeroServer>,
 ) -> Result<NamedFile, Error> {
 	let mut file_path = PathBuf::new();
 	let address = req.match_info().query("address");
@@ -31,10 +33,26 @@ pub fn serve_file(
 
 	trace!(
 		"Serving file: zero://{}/{} as {:?}",
-		req.match_info().query("address"),
-		req.match_info().query("inner_path"),
+		&address,
+		&inner_path,
 		file_path
 	);
+
+	if !file_path.exists() {
+		let lookup =
+			crate::site::site_manager::Lookup::Address(crate::site::address::Address::from_str(address)?);
+		let (_, addr) = block_on(data.site_manager.send(lookup))??;
+		let msg = crate::site::FileGetRequest {
+			inner_path: String::from(inner_path),
+			format: String::new(),
+			timeout: 0f64,
+			required: true,
+		};
+		let res = block_on(addr.send(msg))??;
+		if !res {
+			return Result::Err(Error::MissingError);
+		}
+	}
 
 	let file = NamedFile::open(file_path)?;
 	Result::Ok(file)
